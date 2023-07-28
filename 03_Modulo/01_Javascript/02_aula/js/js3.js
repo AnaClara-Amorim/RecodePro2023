882bff80985230fa6bd1f5a7f6ef028a64f00757
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-07-28 12:07:22 -0300

    update

---
+++ 03_Modulo/01_Javascript/02_aula/js/js3.js
@@ -0,0 +1,21 @@
+// ARRAYS
+
+/* let nome = [];
+
+ nome [0] = "Nome 1";
+ nome [1] = 30;
+ nome [2] = true;
+
+ for (let index = 0; index < nome.length; index++) {
+    alert (nome[index])
+ } */
+
+ let exemplo = [];
+
+for (let i = 0; i < 3; i++);{
+    exemplo[i] = prompt("Digite um nome");
+}
+
+for (let index = 0; index < exemplo.length; index++) {
+    alert (nome[index])
+}
