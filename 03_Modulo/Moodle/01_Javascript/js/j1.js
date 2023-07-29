69fff03c819f3e7a8d4b32d22fad62c607e900d0
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-07-28 23:14:50 -0300

    last commit of the day

---
+++ 03_Modulo/Moodle/01_Javascript/js/j1.js
@@ -0,0 +1,11 @@
+//console.log(window.innerWidth);
+//console.log(screen.width);
+
+window.onresize = function(){
+    console.log(window.innerWidth)  
+    document.write(window.innerWidth)
+};
+
+document.onscroll = function(){
+    console.log ("rolando...")
+}
