cfe8fa6ef3f5bdfeafda2e95aa5e5a089f685a76
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2023-08-02 10:19:30 -0300

    up

---
+++ 03_Modulo/01_Javascript/04_aula/js/exercicio1.js
@@ -0,0 +1,20 @@
+//Crie um script onde o usuário possa digitar nome, sobrenome e idade em prompts diferentes e usando funções de string, o script deve colocar todo conteúdo em uma única variável e tudo em maiúsculo
+
+/* let nome = prompt("Digite seu nome: ")
+let sobrenome = prompt("Digite seu sobrenome: ")
+let idade = prompt("Digite sua idade: ")
+
+let dadosCompletos = alert(nome.toUpperCase() + " " + sobrenome.toUpperCase() + " " + idade) */
+
+// Crie um script em que um usuário possa digitar o nome e o sobrenome no mesmo prompt e você separe em variáveis diferentes, mostrando nome e sobrenome separados
+
+/* let nomeCompleto = prompt("Digite seu nome e seu sobrenome: ")
+let nomee = nomeCompleto.split(" ")
+let sobrenomee = nomeCompleto.split(" ")
+alert ("Nome: " + nomee[0])
+alert("Sobrenome: " + sobrenome[1])
+ */
+
+// Funções matemáticas
+
+
