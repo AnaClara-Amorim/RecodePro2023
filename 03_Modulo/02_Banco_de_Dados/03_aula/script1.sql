c37850b279e5f3c9515b52ac9eb953059f28316e
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-09 12:51:11 -0300

    aula3

---
+++ 03_Modulo/02_Banco_de_Dados/03_aula/script1.sql
@@ -0,0 +1,29 @@
+ create database imobiliaria;
+ use imobiliaria;
+ 
+ create table users (
+ id_user serial primary key,
+ nome varchar (50),
+ email varchar (50),
+ telefone varchar (50),
+ endereco varchar (50)
+ );
+ 
+ create table clientes (
+ id_cliente serial primary key, 
+ nome varchar (50),
+ endereco varchar (50),
+ telefone varchar (20), 
+ email varchar (50)
+ );
+ 
+ insert into users (nome, email, telefone, endereco) 
+ values('Ana do Microondas', 'ana@microondas.com', '123456', 'Rua do Microondas')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
