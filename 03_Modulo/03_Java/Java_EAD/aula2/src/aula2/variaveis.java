47964c113093cbf01f9d17a48752aaffc7ed765d
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-15 16:50:02 -0300

    java

---
+++ 03_Modulo/03_Java/Java_EAD/aula2/src/aula2/variaveis.java
@@ -0,0 +1,19 @@
+package aula2;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int valor; //Criando a variável
+		valor = 2023; //Atribuindo valor			
+		System.out.println(valor); //Mostrando valor
+		
+		double troco = 1.55; //Criando e atribuindo valor
+		System.out.println(troco);
+
+		String titulo;
+		titulo = "Olá, mundo!"; //String entre parenteses
+		System.out.println(titulo);
+	}
+
+}
---
+++ 03_Modulo/03_Java/Java_EAD/teste/src/teste/javaTeste.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class javaTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, Mundo!");
+	}
+
+}
