0712a08a628490275fc957c96a44c860d92c01a1
Ana Clara <anaamorimsantos1@gmail.com>
2023-08-18 10:11:11 -0300

    Update Exercícios.java

--- 03_Modulo/03_Java/02_aula/aula02/src/aula02/Exercícios.java
+++ 03_Modulo/03_Java/02_aula/aula02/src/aula02/Exercícios.java
@@ -5,21 +5,19 @@ public class Exercícios {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("Digite a quantidade de minutos utilizada: ");
-		double minutosUtilizados = entrada.nextDouble();
-		
-		if (minutosUtilizados <= 100) {
-			System.out.println("O preço a se pagar é de R$ 50!");			
-		} else {
-			double valorExcedente;
-			double valorFinal;
-			valorExcedente = minutosUtilizados - 100;
-			valorFinal = valorExcedente * 2 + 50;
-			System.out.println("O preço a se pagar é de R$: " + valorFinal);			
-			
-		}
-		
-	}
 
+		System.out.println("===Simulador de faturas===");
+		System.out.println("Informe quantiidade de minutos utilizada: ");
+			int minutos = entrada.nextInt();
+
+			int minexcedente = 0;
+			double valorFinal = (double) 50 ;
+
+		if (minutos > 100) {
+				minexcedente = (minutos - 100) * 2;
+		}
+			valorFinal += minexcedente;
+		System.out.printf("Você utilizou %d minutos.\nSua fatura custará: R$%.2f", minutos, valorFinal);
+
+	}
 }
