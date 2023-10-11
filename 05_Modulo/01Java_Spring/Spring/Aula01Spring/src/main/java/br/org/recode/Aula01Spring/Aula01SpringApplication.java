c0295db74b4c53590ff55288c2f6c1b630997865
Ana Clara <anaamorimsantos1@gmail.com>
2023-10-11 12:15:40 -0300

    JavaSpring

---
+++ 05_Modulo/01Java_Spring/Spring/Aula01Spring/src/main/java/br/org/recode/Aula01Spring/Aula01SpringApplication.java
@@ -0,0 +1,13 @@
+package br.org.recode.Aula01Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01SpringApplication.class, args);
+	}
+
+}
---
+++ 05_Modulo/01Java_Spring/Spring/Aula01Spring/src/test/java/br/org/recode/Aula01Spring/Aula01SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.recode.Aula01Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
